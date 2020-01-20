76786a6dc437761ef88ba4c9de594966537e2644
TsengYiYao <yiyatsen@gmail.com>
2020-01-20 10:51:13 +0800

    200. Working with Multiple Providers [finished]

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -19,14 +19,14 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
   }
 
   int get itemCount {
-    return _items != null ? _items.length : 0;
+    return _items.length;
   }
 
   void addItem(String productId, double price, String title) {
