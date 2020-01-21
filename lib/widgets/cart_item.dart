4d3cb287be0c4ebe886a200ab93a0fb8f372c039
TsengYiYao <yiyatsen@gmail.com>
2020-01-21 10:47:50 +0800

    204. Making Cart Items Dismissible - finished

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:my_shop/providers/cart.dart';
 
 class CartItem extends StatelessWidget {
+  final String productId;
   final String id;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem({this.id, this.price, this.quantity, this.title});
+  CartItem(
+      {@required this.productId,
+      @required this.id,
+      @required this.price,
+      @required this.quantity,
+      @required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +22,19 @@ class CartItem extends StatelessWidget {
       key: ValueKey(id),
       background: Container(
         color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (d) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
       child: Card(
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         child: Padding(
