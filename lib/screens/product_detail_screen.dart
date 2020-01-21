d64e505d4b2c3822ba9863d25079f608f447f779
TsengYiYao <yiyatsen@gmail.com>
2020-01-21 11:28:23 +0800

    205. Adding Product Detail Data - finished

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -3,10 +3,6 @@ import 'package:my_shop/providers/products_provider.dart';
 import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-//  final String title;
-//  final String price;
-//
-//  ProductDetailScreen(this.title, this.price);
   static const routeName = '/product-detail';
 
   @override
@@ -20,6 +16,32 @@ class ProductDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(loadedProduct.description,
+                  textAlign: TextAlign.center, softWrap: true),
+            ),
+          ],
+        ),
       ),
     );
   }
